39a0748a0ec62e4b3abef0f4621725ae393a3fd3
Odair Cesar Ferreira <odairferreira97@gmail.com>
2022-02-01 14:07:39 -0300

    Adicionando fundo ao menu quando acontece um scroll, criando a seção home e adicionando um ação ao menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 /*---------------------- Show Menu ----------------------*/
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close')
+  navClose = document.getElementById('nav-close'),
+  navLink = document.querySelectorAll('.nav__link'),
+  header = document.getElementById('header')
 
 if (navMenu) {
   /* Menu Show */
@@ -18,3 +20,23 @@ if (navMenu) {
     })
   }
 }
+
+/*---------------------- Remove Menu Mobile ----------------------*/
+function linkAction(){
+  if(navMenu){
+    navMenu.classList.remove('show-menu')
+  }
+}
+
+if(navLink){
+  navLink.forEach(n => n.addEventListener('click', linkAction))
+}
+
+/*---------------------- Change Background Header ----------------------*/
+function scrollHeader(){
+  if(header){
+    this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header')
+  }
+}
+
+window.addEventListener('scroll', scrollHeader)
