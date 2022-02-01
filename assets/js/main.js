76995cfd357f6b4bf73a79b13cdd29505e918ac0
Odair Cesar Ferreira <odairferreira97@gmail.com>
2022-02-01 13:23:02 -0300

    Criando o menu.

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/*---------------------- Show Menu ----------------------*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
+
+if (navMenu) {
+  /* Menu Show */
+  if (navToggle) {
+    navToggle.addEventListener('click', () => {
+      navMenu.classList.add('show-menu')
+    })
+  }
+
+  /* Menu Hidden */
+  if (navClose) {
+    navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu')
+    })
+  }
+}
