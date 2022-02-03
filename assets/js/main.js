18ce7f9526b4521ff6b8d6f70a177fd90cb8b0ec
Odair Cesar Ferreira <odairferreira97@gmail.com>
2022-02-02 22:16:36 -0300

    Adicionando Scroll Up, Scroll Bar e colocando efeito no menu.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
   navClose = document.getElementById('nav-close'),
   navLink = document.querySelectorAll('.nav__link'),
-  header = document.getElementById('header')
+  header = document.getElementById('header'),
+  sections = document.querySelectorAll('section[id]')
 
 if (navMenu) {
   /* Menu Show */
@@ -38,5 +39,29 @@ function scrollHeader(){
     this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header')
   }
 }
-
 window.addEventListener('scroll', scrollHeader)
+
+/*---------------------- Show Scroll Up ----------------------*/
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up')
+  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  this.scrollY >= 200 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*---------------------- Scroll Sections Active Link ----------------------*/
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id')
+
+    scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ?
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+      :
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+  })
+}
+window.addEventListener('scroll', scrollActive)
